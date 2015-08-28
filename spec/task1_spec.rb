a77e519c3f400a3b90d91591ea68536785554087
kolesnikov <re.vkolesnikov@gmail.com>
2015-08-28 14:19:31 +0300

    Update Task1 solution

--- spec/task1_spec.rb
+++ spec/task1_spec.rb
@@ -1,30 +1,19 @@
 require_relative "../tasks/task1"
 
 describe Task1 do
-  describe ".solution_stack" do
+  describe ".solution" do
     let(:long_valid_str) { "()" * 1000000 }
     let(:long_invalid_str) { "#{long_valid_str})" }
 
-    it { expect(Task1.solution_stack("")).to be true }
-    it { expect(Task1.solution_stack("()()()")).to be true }
-    it { expect(Task1.solution_stack("()()(")).to be false }
-    it { expect(Task1.solution_stack("()())")).to be false }
-    it { expect(Task1.solution_stack("()()))(")).to be false }
-    it { expect(Task1.solution_stack("(")).to be false }
-    it { expect(Task1.solution_stack(")")).to be false }
-    it { expect(Task1.solution_stack(")(")).to be false }
-    it { expect(Task1.solution_stack(long_valid_str)).to be true }
-    it { expect(Task1.solution_stack(long_invalid_str)).to be false }
-  end
-
-  describe ".solution_recursive" do
-    it { expect(Task1.solution_recursive("")).to be true }
-    it { expect(Task1.solution_recursive("()()()")).to be true }
-    it { expect(Task1.solution_recursive("()()(")).to be false }
-    it { expect(Task1.solution_recursive("()())")).to be false }
-    it { expect(Task1.solution_recursive("()()))(")).to be false }
-    it { expect(Task1.solution_recursive("(")).to be false }
-    it { expect(Task1.solution_recursive(")")).to be false }
-    it { expect(Task1.solution_recursive(")(")).to be false }
+    it { expect(Task1.solution("")).to be true }
+    it { expect(Task1.solution("()()()")).to be true }
+    it { expect(Task1.solution("()()(")).to be false }
+    it { expect(Task1.solution("()())")).to be false }
+    it { expect(Task1.solution("()()))(")).to be false }
+    it { expect(Task1.solution("(")).to be false }
+    it { expect(Task1.solution(")")).to be false }
+    it { expect(Task1.solution(")(")).to be false }
+    it { expect(Task1.solution(long_valid_str)).to be true }
+    it { expect(Task1.solution(long_invalid_str)).to be false }
   end
 end
--- tasks/task2.rb
+++ tasks/task2.rb
@@ -1,3 +1,4 @@
+# See task description in README.md
 class Task2
   def self.solution(s)
     ch_map = s.chars.each_with_object({}) { |ch, m| m[ch] = m.fetch(ch, 0) + 1 }
